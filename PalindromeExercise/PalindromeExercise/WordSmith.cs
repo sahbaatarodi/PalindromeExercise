3536b0ca96eb6d5127746103ae78f6158b9a1ee2
sahba atarodi <sahba.atarodi@gmail.com>
2023-12-19 18:01:16 -0500

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+
+            if (reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
